f381f2969522b6a34d8227eb5816b2bf289e7a51
Bouchrra <aichabouchraa4@gmail.com>
2024-01-19 23:35:22 +0100

    La création de notre model, contenant l'objet Weather.

---
+++ lib/Models/weather.dart
@@ -0,0 +1,23 @@
+class Weather {
+  String city;
+  String pays;
+  double tem;
+  int pre;
+  int hum;
+  String des;
+  double wind;
+  String iconCode;
+  String iconUrl;
+
+  Weather({
+    required this.city,
+    required this.pays,
+    required this.tem,
+    required this.pre,
+    required this.hum,
+    required this.des,
+    required this.wind,
+    required this.iconCode,
+    required this.iconUrl,
+  });
+}
