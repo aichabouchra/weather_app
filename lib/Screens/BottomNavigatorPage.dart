ae622090e93f7ab01c6e259a57ea7fc7cbf196bf
Bouchrra <aichabouchraa4@gmail.com>
2024-01-19 23:43:47 +0100

    La création de la widget Bottom Bar Widget, pour personnaliser la barre en bas, et faciliter la navigation entre les screens.

---
+++ lib/Screens/BottomNavigatorPage.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+import 'HomeWeatherPage.dart';
+import 'SearchWeatherPage.dart';
+import 'SettingsWeatherPage.dart';
+
+class BottomNavigatorPage extends StatefulWidget {
+  @override
+  _BottomNavigatorPageState createState() => _BottomNavigatorPageState();
+}
+
+class _BottomNavigatorPageState extends State<BottomNavigatorPage> {
+  int _currentIndex = 0;
+
+  final List<Widget> _pages = [
+    HomeWeatherPage(),
+    SearchWeatherPage(),
+    SettingsWeatherPage(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _pages[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home_outlined),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search_outlined),
+            label: 'Search',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Settings',
+          ),
+        ],
+      ),
+    );
+  }
+}
